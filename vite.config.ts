568487d301c67d21f1f3ba4ef92e0b8cce827892
Mitfart <mr.mitfart@mail.ru>
2026-05-26 15:30:32 +0300

    Git Page Fix v2

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/Owlbear_Kanban/",
+  base: "/Owlbear_Board/",
   plugins: [react()],
   build: {
     outDir: "dist",
